9c89451ace8340273ab0975331725b1cf1a644b4
Donny Chan <chenyingfa1999@qq.com>
2023-06-02 00:20:33 +0800

    App.vue formatted to Composition API for better code

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,66 @@
-<script lang="ts">
+<script setup lang="ts">
 import Editor from './components/Editor.vue'
 import TitleBar from './components/TitleBar.vue'
 import StatusBar from './components/StatusBar.vue'
 
-const { ipcRenderer } = require('electron')
-const fs = require('fs')
+import type { Ref } from 'vue'
+import { ref } from 'vue'
 
+const { ipcRenderer } = require('electron')  // Somehow this can't be changed to import
+import * as fs from 'fs'
 
-export default {
-    name: 'App',
-    components: {
-        TitleBar,
-        Editor,
-        StatusBar,
-    },
-    data() {
-        return {
-            fileName: 'Untitled-1',
-            isSelectedFile: false,
-        }
-    },
-    methods: {
-        openFile(): void {
-            ipcRenderer.invoke('open-file')
-                .then((paths: string[]) => {
-                    console.log('got files:', paths)
-                    if (paths.length > 0) {
-                        const path = paths[0]
-                        fs.readFile(path, 'utf-8', (err: any, data: string) => {
-                            if (err) {
-                                console.error(err)
-                                return
-                            }
-                            console.log(data)
-                        })
-                    }
-                })
-        },
-        saveFile(): void {
-            if (this.isSelectedFile) {
-                this.saveToFile(this.fileName)
-            } else {
-                this.saveAs()
-            }
-            return
-        },
-        saveAs(): void {
-            ipcRenderer.invoke('save-file').then((path: string) => {
-                console.log('got path:', path)
-                if (path.length > 0) {
-                    this.saveToFile(path)
-                }
-            })
-        },
-        saveToFile(path: string): void {
-            console.log('saving to file:', path)
-            let editor = this.getEditor()
-            let content = editor.getAllContents()
-            fs.writeFile(path, content, (err: any) => {
+let fileName = ref('Untitled-1')
+let isSelectedFile = ref(false)
+let editor: Ref<typeof Editor | null> = ref(null)
+
+function openFile() {
+    ipcRenderer.invoke('open-file').then((paths: string[]) => {
+        console.log('got files:', paths)
+        if (paths.length > 0) {
+            const path = paths[0]
+            fs.readFile(path, 'utf-8', (err: any, data: string) => {
                 if (err) {
                     console.error(err)
                     return
                 }
-                console.log('file saved')
-                this.fileName = path
-                this.isSelectedFile = true
+                console.log(data)
             })
-        },
+        }
+    })
+}
 
-        getEditor(): typeof Editor {
-            return this.$refs.editor as typeof Editor
-        },
+function saveFile() {
+    if (isSelectedFile) {
+        saveToFile(fileName.value)
+    } else {
+        saveAs()
     }
+    return
+}
+
+function saveAs() {
+    ipcRenderer.invoke('save-file').then((path: string) => {
+        console.log('got path:', path)
+        if (path.length > 0) {
+            saveToFile(path)
+        }
+    })
+}
+function saveToFile(path: string): void {
+    console.log('saving to file:', path)
+    let content = ''
+    if (editor.value !== null) {
+        content = editor.value.getAllContents()
+    }
+    fs.writeFile(path, content, (err: any) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log('file saved')
+        fileName.value = path
+        isSelectedFile.value = true
+    })
 }
 
 </script>
@@ -79,11 +68,8 @@ export default {
 <template>
     <link href="../../styles/main.css" rel="stylesheet">
     <div id="root-container">
-        <TitleBar title="Qingshu (beta)" 
-                    :fileName="fileName"
-                    @open-file="openFile"
-                    @save-file="saveFile"
-                    @save-as="saveAs" />
+        <TitleBar title="Qingshu (beta)" :fileName="fileName" @open-file="openFile" @save-file="saveFile"
+            @save-as="saveAs" />
         <Editor id="editor-container" ref="editor" />
         <StatusBar />
     </div>
